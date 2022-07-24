43d80fa3deefdf818dcd1b1f0641ba40caad0171
Eng.Tariq <109592191+Eng6ro8@users.noreply.github.com>
2022-07-24 09:55:47 +0300

    editor page html
    css + javasecript

---
+++ T2/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+<script>
+    function ddlselect() {
+            var d = document.getElementById("ddselect");
+    var displaytext = d.options[d.selectedIndex].text;
+    document.getElementById("txtvalue").value = displaytext;
+        }
+</script>
+// Write your Javascript code.
